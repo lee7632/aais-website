17bf363f992b0ae9bba67da78667ee1b611d584f
lee7632 <lee7632@vandals.uidaho.edu>
2018-11-15 08:56:35 -0700

    Update ReactCalendar.js
    event update: Nov 16 movie night

--- aais-site/src/components/ReactCalendar.js
+++ aais-site/src/components/ReactCalendar.js
@@ -46,7 +46,7 @@ const aaisEvents = [
         allDay: false,
         startDate: new Date('November 16, 2018 17:00:00'),
         endDate: new Date('November 16, 2018 18:00:00'),
-        title: 'Meeting HRBB 113',
+        title: 'Movie Night (A.I. Artificial Intelligence directed by Stephen Spielberg)',
     },
     {
         allDay: false,
